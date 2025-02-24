c847c6c76ceaadf5af78159780d785b9f6f82e8e
THENONT123_ <Nonthaphatc@hotmail.com_>
2025-02-25 02:42:06 +0700

    finish

--- lab22.cpp
+++ lab22.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cmath>
 using namespace std;
+#define PI 3.14159265
 
 class ComplexNumber{				
 	public:
@@ -14,6 +15,7 @@ class ComplexNumber{
 		bool operator==(const ComplexNumber &);
 		double abs();
 		double angle();
+
 };
 
 ComplexNumber::ComplexNumber(double x = 0,double y = 0){
@@ -24,11 +26,60 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 	return ComplexNumber(real+c.real,imag+c.imag);
 }
 
+ComplexNumber operator+(double s, const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
-//Write your code here
+ComplexNumber operator-(double s, const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber operator*(double s, const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber&c){
+	return ComplexNumber((real*c.real)-(imag*c.imag), (real*c.imag)+(imag*c.real));
+}
+
+ComplexNumber operator/(double s, const ComplexNumber &c){
+	double abs = pow(c.real, 2)+pow(c.imag, 2);
+	return ComplexNumber((s*c.real/abs), (s*-c.imag/abs));
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+	double abs = pow(c.real, 2)+pow(c.imag, 2);
+	return ComplexNumber((real*c.real/abs)+(imag*c.imag/abs), (imag*c.real/abs)+(real*-c.imag/abs));
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+	if(real==c.real && imag == c.imag)return true;
+	else return false;
+}
+bool operator==(const ComplexNumber&c1 , const ComplexNumber&c2){
+	if(c1.real == c2.real && c1.imag == c2.imag) return true;
+	else return false;
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real) * 180 / PI;
+}
+
+ostream & operator<<(ostream& os , const ComplexNumber &c){
+	if(c.real == 0 && c.imag == 0) return os << 0;
+	else if(c.real != 0 && c.imag == 0) return os << c.real;
+	else if(c.real == 0 && c.imag != 0) return os << c.imag <<"i";
+	else if(c.imag > 0) return os << c.real << "+" << c.imag <<"i";
+	else return os << c.real << c.imag <<"i";
+}
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
